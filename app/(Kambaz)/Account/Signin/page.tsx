840dc928a450cffecf7aa786054ebc6d6889b4e4
Asana Tadayon Mousavi <tadayonmousavi.a@northeastern.edu>
2025-10-07 22:36:28 -0400

    updates

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import FormControl from 'react-bootstrap/FormControl';
 export default function Signin() {
  return (
    <div id="wd-signin-screen">
      <h3>Sign in</h3>
-     <input placeholder="username" className="wd-username" /> <br />
-     <input placeholder="password" type="password" className="wd-password" /> <br />
-     <Link href="/Dashboard" id="wd-signin-btn"> Sign in </Link> <br />
+     <FormControl placeholder="username" className="mb-2" id="wd-username" />
+     <FormControl placeholder="password" type="password" className="mb-2" id="wd-password" />
+     <Link href="/Dashboard" id="wd-signin-btn" className="btn btn-primary w-100 mb-2"> Sign in </Link>
      <Link href="Signup" id="wd-signup-link"> Sign up </Link>
    </div>
 );}
--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import FormControl from 'react-bootstrap/FormControl';
 export default function Signup() {
  return (
    <div id="wd-signup-screen">
      <h3>Sign up</h3>
-     <input placeholder="username" className="wd-username" /> <br />
-     <input placeholder="password" type="password" className="wd-password" /> <br />
-     <input placeholder="verify password" type="password" className="wd-password-verify" /> <br />
-     <Link href="Profile" id="wd-signup-btn"> Sign up </Link> <br />
+     <FormControl placeholder="username" className="mb-2" id="wd-username" />
+     <FormControl placeholder="password" type="password" className="mb-2" id="wd-password" />
+     <FormControl placeholder="verify password" type="password" className="mb-2" id="wd-password-verify" />
+     <Link href="Profile" id="wd-signup-btn" className="btn btn-primary w-100 mb-2"> Sign up </Link>
      <Link href="Signin" id="wd-signin-link"> Sign in </Link>
    </div>
 );}
